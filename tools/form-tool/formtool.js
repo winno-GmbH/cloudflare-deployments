957558ab6f284bdb80b4ae3f73bcfed0140eb8ab
Besson <yanic.besson@gmail.com>
2024-08-26 00:05:22 +0200

    test for form tool

--- tools/form-tool/formtool.js
+++ tools/form-tool/formtool.js
@@ -13,7 +13,7 @@ const accessKey = urlParams.get("key") ?? "fd821fc7-53b3-4f4c-b3b0-f4adf10491c7"
 const formName = urlParams.get("form") ?? "Testformular";
 const captchaKey = urlParams.get("captcha-key");
 
-console.log("Form Submit v0.3.29");
+console.log("Form Submit v0.3.30");
 
 const serverUrl = "https://gecko-form-tool-be-new.vercel.app/api/forms/submit";
 
@@ -495,6 +495,7 @@ if (form) {
       formStep: form,
       formStepNumber: 0,
       name: "Form Data",
+      id: "",
     });
   }
 
