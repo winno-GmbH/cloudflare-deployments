140bd035bfd04453ee0f52b855c39fe265d656d8
Besson <yanic.besson@gmail.com>
2024-08-25 15:57:25 +0200

    test for form tool

--- tools/form-tool/formtool.js
+++ tools/form-tool/formtool.js
@@ -13,7 +13,7 @@ const accessKey = urlParams.get("key") ?? "fd821fc7-53b3-4f4c-b3b0-f4adf10491c7"
 const formName = urlParams.get("form") ?? "Testformular";
 const captchaKey = urlParams.get("captcha-key");
 
-console.log("Form Submit v0.3.16");
+console.log("Form Submit v0.3.17");
 
 const serverUrl = "https://gecko-form-tool-be-new.vercel.app/api/forms/submit";
 
@@ -196,7 +196,7 @@ const formSteps = () => {
       formStep.formStepNumber.classList.remove("completed");
       formStep.formStepNumber.classList.remove("active");
       formStep.formStepNumber.classList.remove("locked");
-      if (formStep.formStep.id !== "" && !formStep.formStep.getAttribute("condition-active") === "true") {
+      if (formStep.formStep.id !== "" && formStep.formStep.getAttribute("condition-active") !== "true") {
         formStep.formStepNumber.classList.add("hidden");
       } else {
         formStep.formStepNumber.classList.remove("hidden");
@@ -241,7 +241,6 @@ const formSteps = () => {
       formStep.querySelectorAll("input[type=checkbox], input[type=radio]").forEach((input) => {
         if (input.getAttribute("conditional-step")) {
           input.addEventListener("change", (e) => {
-            console.log(e.target.checked);
             const conditionalSteps = input.getAttribute("conditional-step").replace(" ", "").split(",");
             formStepPairs.map((formStep) => {
               if (e.target.checked) {
