1d61641d927ff614ed0e68e05391ac9864b039e1
Besson <yanic.besson@gmail.com>
2025-03-03 16:18:16 +0100

    webpack deployment

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./dev/form-tool/form-tool-v2/formtool-v2.ts",
+  output: {
+    filename: "formtool-test-v2.js",
+    path: path.resolve(__dirname, "tools/form-tool"),
+    library: "FormTool",
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+};
