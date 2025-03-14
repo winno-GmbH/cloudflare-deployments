2ad6e989b53604a0ba2e5fd9772100f1be91f770
Besson <yanic.besson@gmail.com>
2025-03-14 20:17:58 +0100

    push new version

--- dev/form-tool/form-tool-v2/formtool-v2.ts
+++ dev/form-tool/form-tool-v2/formtool-v2.ts
@@ -21,7 +21,7 @@ class FormTool {
     this.formName = urlParams.get("form") ?? "Testformular";
     this.captchaKey = urlParams.get("captcha-key");
 
-    console.log("Form Submit v0.2.36");
+    console.log("Form Submit v0.2.37");
 
     this.form = document.querySelector(`[name="${this.formName}"]`);
   }
