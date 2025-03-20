be57800b017ef81a61f4ffd76b138799c97cc316
Besson <yanic.besson@gmail.com>
2025-03-20 10:59:03 +0100

    push new version

--- dev/form-tool/form-tool-v2/formtool-v2.ts
+++ dev/form-tool/form-tool-v2/formtool-v2.ts
@@ -25,7 +25,7 @@ class FormTool {
 
     this.sessionId = this.generateSessionId();
 
-    console.log("Form Submit v0.2.44");
+    console.log("Form Submit v0.2.45");
 
     this.form = document.querySelector(`[name="${this.formName}"]`);
   }
