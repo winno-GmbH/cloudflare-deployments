c3f981c165753276d894fd0f63097b8b7852d407
Besson <yanic.besson@gmail.com>
2025-04-07 10:49:22 +0200

    push new version

--- dev/form-tool/form-tool-v2/formtool-v2.ts
+++ dev/form-tool/form-tool-v2/formtool-v2.ts
@@ -25,7 +25,7 @@ class FormTool {
 
     this.sessionId = this.generateSessionId();
 
-    console.log("Form Submit v0.2.58");
+    console.log("Form Submit v0.2.59");
 
     this.form = document.querySelector(`[name="${this.formName}"]`);
   }
@@ -178,9 +178,9 @@ class FormTool {
         } else {
           ct.classList.remove("checked");
         }
-        this.form?.querySelectorAll(`input[name="${input.name}"]`).forEach((checkbox) => {
-          checkbox.dispatchEvent(new Event("change"));
-        });
+        // this.form?.querySelectorAll(`input[name="${input.name}"]`).forEach((checkbox) => {
+        //   checkbox.dispatchEvent(new Event("change"));
+        // });
       });
     });
 
